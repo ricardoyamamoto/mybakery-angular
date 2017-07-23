a02310a37da078d023fd33abfaa8e64e237038f2
Luis Gutierrez <lfgcas@outlook.com>
2017-07-22 20:24:39 -0400

    Edit and Add Ingredient

---
+++ src/app/ingredient/edit-ingredient/edit-ingredient.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import {Router} from '@angular/router';
+import {IngredientService} from '../../services/ingredient.service';
+import {Unit} from "../../models/unit";
+import {Ingredient} from "../../models/ingredient";
+
+
+@Component({
+  selector: 'app-edit-ingredient',
+  templateUrl: './edit-ingredient.component.html',
+  styleUrls: ['./edit-ingredient.component.css'],
+  providers: [IngredientService]
+})
+export class EditIngredientComponent implements OnInit {
+
+  @Input() units: Unit[];
+  @Input() myIngredient: Ingredient = new Ingredient();
+  @Input() myUnit: Unit = new Unit();
+  @ViewChild('unitTextBox2') unitTextBox;
+
+  constructor(private router: Router, private ingredientListService: IngredientService) { }
+
+  ngOnInit() {
+  }
+
+  infoUnit(unit: Unit) {
+    this.myUnit = unit;
+    this.myIngredient.defaultUnit = this.myUnit._id;
+    this.unitTextBox.value = unit.shortDescription;
+  }
+
+  saveEditIngredient() {
+
+    this.ingredientListService.editData(this.myIngredient).subscribe(data => console.log(data));
+
+
+  }
+
+}
